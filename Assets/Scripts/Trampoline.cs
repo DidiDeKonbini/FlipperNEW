6cba55f42d56c1ee41179521b6a2bea291b2b05b
Yvan Delhaye <y.delhaye@rubika-edu.com>
2025-11-26 20:49:27 +0100

    .

---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    [SerializeField] int strength = 35;
+    public int scoreToAdd = 10;
+    public Animation anim;
+    public GameObject bounceDirection;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Rigidbody rb  =  other.GetComponent<Rigidbody>();
+            Vector3   dir = (bounceDirection.transform.position - transform.position).normalized;
+            rb.AddForce(dir * strength, ForceMode.Impulse);   
+        }
+
+        ScoreManager.instance.AddScore(scoreToAdd);
+        //anim.Play();
+    }
+}
